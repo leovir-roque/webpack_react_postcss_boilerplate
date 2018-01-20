69ab525213045094e0580219a54b62a53b863e85
Leovir Roque <lroque@freelancer.com>
2018-01-20 18:57:36 +0800

    CSS cleanups

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var emotions = require('./routes/emotions');
 
 var app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/emotions', emotions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Leovir Roque | Emotion Analysis' });
+  res.render('index', { title: 'Webpack + ReactJS + PostCSS Boilerplate' });
 });
 
 module.exports = router;
