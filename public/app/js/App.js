ebf3432308f7bcfd7c4ce564c2b1da9aef5d5af2
Leovir Roque <lroque@freelancer.com>
2018-01-20 19:58:21 +0800

    Added example + more cleanups

--- public/app/js/App.js
+++ public/app/js/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDom from 'react-dom';
-import Acts from './components/Acts.js';
+import SamplePage from './containers/SamplePage.js';
 
-class App extends Component{
+class App extends React.Component {
     render() {
         return (
-            <Acts />
+            <SamplePage />
         )
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Webpack + ReactJS + PostCSS Boilerplate' });
+  res.render('index', { title: 'Hello! Webpack + ReactJS + PostCSS Boilerplate' });
 });
 
 module.exports = router;
